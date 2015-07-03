723a900ce0529e78f6f2041024d10ac6e54caec0
Deepeloper <weregod@gmail.com>
2015-07-03 14:17:39 +0700

    Composer instructions added.

---
+++ app/classes/AmiLabs/PingReports/Composer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AmiLabs\PingReports;
+
+use Composer\Script\Event;
+
+/**
+ * Composer event handlers.
+ *
+ * @package AmiLabs\SigningService
+ */
+class Composer{
+    /**
+     * @param  Event $oEvent
+     * @return void
+     */
+    public static function postInstall(Event $oEvent){
+        self::postUpdate($oEvent);
+    }
+
+    /**
+     * @param  Event $oEvent
+     * @return void
+     */
+    public static function postUpdate(Event $oEvent){
+        @chmod('./db', 0777);
+        @chmod('./db/ping_reports.db', 0777);
+
+        // Update web folders access mode
+        // @chmod('./web/css', 0777);
+        // @chmod('./web/js', 0777);
+    }
+}
